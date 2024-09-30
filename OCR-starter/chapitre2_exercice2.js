f587b73ce75f72f87f4b213942a8c665279548bb
Hugo Girona <hugo.girona@student.hepl.be>
2024-09-30 14:18:50 +0200

    Update chapitre2_exercice2.js

--- OCR-starter/chapitre2_exercice2.js
+++ OCR-starter/chapitre2_exercice2.js
@@ -9,7 +9,7 @@
 // Initialisez la variable celsius avec une valeur de votre choix
 let celsius = 32;
 // Calculez la température équivalente en degrés Fahrenheit et stockez-la dans la variable fahrenheit
-fahrenheit = (celsius * 9/5) + 32;
+let fahrenheit = (celsius * 9/5) + 32;
 // Affichez la température en degrés Celsius et sa conversion en degrés Fahrenheit dans la console en utilisant une phrase descriptive
 console.log(`quand la température est de ${celsius} degrés celsius, celà équivaut à une température de ${fahrenheit} degrés Fahrenheit.`);
 // Modifiez la valeur de la variable celsius
